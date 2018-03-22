2c9b5a8d742cf162e757f3a81b540c42c1b68b2b
Stu Ellerbusch <stu.ellerbusch@gmail.com>
2018-03-21 20:41:19 -0400

    Start of Sample

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
-# itscode
+## itsjustcode
 
-Python based SQL Alchemy Database First Code Generator
+Database First Code Generator that uses the [SQL Alchemy][sqlalchemy] and [Jinja2][jinja2].
+
+### It's Just Code - Generate It
+
+*itsjustcode* is intended to generate code from Jinja templates using SQL Alchemy's metadata. If
+you are looking for a library to generate class models from an existing database, this isn't
+it ... try [sqlacodegen][sqlacodegen]. *itsjustcode* is inteded to provide boilerplate code from templates to flesh out HTML files, or baseline javascript files for any number of available JavaScript frameworks (or TypeScript).
+
+At the present moment, *itsjustcode* requires access to an existing database where it has been
+created via SQL Alchemy models (or created with regular SQL scripts). *itsjustcode* uses SQL Alchemy
+to access the metadata of a database in a conistent manner.
+
+### Sample Usage
+
+Generate boilerplate code either by passing in a template for a path to a template file.
+
+```python
+from itsjustcode import generateit
+
+generateit(db_uri='mysql://username:password@localhost/database',
+           table='table_name',
+           template_file='./code_templates/list.html',
+           out_file='../myweb/static/table_name.list.html')
+```
 
 https://python-packaging.readthedocs.io/en/latest/
+
+[sqlalchemy]: http://www.sqlalchemy.org/
+[jinja2]: http://jinja.pocoo.org/
+[sqlacodegen]: https://pypi.python.org/pypi/sqlacodegen
--- itsjustcode/__init__.py
+++ itsjustcode/__init__.py
@@ -1,3 +1,7 @@
+import __main__
+
+import os
+
 from sqlalchemy import MetaData, create_engine
 from jinja2 import Template
 from jinja2.filters import FILTERS
@@ -15,33 +19,55 @@ FILTERS['snake_case'] = snake_case
 FILTERS['camel_case'] = camel_case
 FILTERS['pascal_case'] = pascal_case
 
-__all__ = ('generateit')
+def get_absolute_path(path):
+    full_path = path
+    if not os.path.isabs(full_path):
+        dir_path = os.path.dirname(__main__.__file__)
+        full_path = os.path.join(dir_path, path)
+    return full_path
 
-
-def generateit(sqlalchemy_url, table, template_file=None, template_contents=None):
+def generateit(**kwargs):
     '''
     Generate - blah de blah
 
-    :param sqlalchemy_url:
+    :param engine:
+
+    :param db_uri:
 
     :param table:
 
     :param template_file:
 
     :param template_contents:
+
+    :param out_file:
     '''
+
+    engine = kwargs.pop('engine', None)
+    db_uri = kwargs.pop('db_uri', None)
+    table = kwargs.pop('table', None)
+    template_file = kwargs.pop('template_file', None)
+    template_contents = kwargs.pop('template_contents', None)
+    out_file = kwargs.pop('out_file', None)
+
+    if engine is None and db_uri is None:
+        raise ValueError('engine or db_uri must be provided')
+
     if template_file is None and template_contents is None:
         raise ValueError('template_file or template_contents must be provided')
 
-    engine = create_engine(sqlalchemy_url)
+    if engine is None:
+        engine = create_engine(db_uri)
 
     metadata = MetaData(engine)
     metadata.reflect(only=[table])
 
     if template_contents is None:
-        with open(template_file, 'r') as f:
+        template_file_path = get_absolute_path(template_file)
+        with open(template_file_path, 'r') as f:
             template_contents = f.read()
 
     results = Template(template_contents).render(table=metadata.tables[table])
 
-    return results
+    with open(out_file, 'w') as f:
+        f.write(results)
